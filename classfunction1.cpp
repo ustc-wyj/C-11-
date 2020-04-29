fb234542dd137542db9ad2ce2708f21a1162816e
ustc-wyj <1910146885@qq.com>
2020-04-29 23:05:02 +0800

    Add files via upload

---
+++ classfunction1.cpp
@@ -0,0 +1,20 @@
+#include <thread>
+#include <iostream>
+
+using namespace std;
+
+// 3.functor (Funciton Object)
+class Base {
+public:
+    void operator()(int x) {
+        while (x-- > 0) {
+            cout << x << endl;
+        }
+    }
+};
+
+int main() {
+    thread t(Base(), 10);
+    t.join();
+    return 0;
+}
---
+++ static_member_function.cpp
@@ -0,0 +1,20 @@
+include <thread>
+#include <iostream>
+
+using namespace std;
+
+// 4.Non-static member function
+class Base {
+public:
+    static void fun(int x) {
+        while (x-- > 0) {
+            cout << x << endl;
+        }
+    }
+};
+
+int main() {
+    thread t(&Base::fun, 10);
+    t.join();
+    return 0;
+}
